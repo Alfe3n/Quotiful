1a663289436d00ec1c66a75d11358ccc13e48ac0
Alfe3n <115562186+Alfe3n@users.noreply.github.com>
2023-05-19 23:03:34 +0530

    Bookmarks page layout done
    To do
    remove bookmark feature from bookmark page

--- src/Pages/Bookmark.jsx
+++ src/Pages/Bookmark.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { BsFillBookmarkCheckFill } from 'react-icons/bs'
 
 function Bookmark() {
+  const bookmarks = useSelector((state) => state.bookmarklist.bookmark)
+
   return (
-    <div>
+    <div className="flex flex-col items-center min-h-screen page-container bg-violet">
       <Navbar bookmark={true} />
-      <p>hello</p>
+      {bookmarks[0] == undefined ? (
+        <h1 className="mt-20 text-2xl font-extrabold cursor-pointer text-red md:text-6xl">No Bookmarks!</h1>
+      ) : (
+        <>
+          {bookmarks.map((quote) => {
+            return (
+              <div
+                key={quote._id}
+                className="flex flex-col items-center justify-around w-5/6 p-4 mb-10 leading-relaxed text-white md:w-2/3 md:p-8 quote-container bg-red rounded-3xl gap-y-4 md:gap-y-8 md:leading-loose">
+                <p className="text-lg text-center md:text-xl lg:2xl">{quote.content}</p>
+                <div className="flex items-center justify-between w-full">
+                  <p className="hidden text-red md:block">invisible</p>
+                  <p className="font-extrabold text-left md:text-lg lg:xl">~{quote.author}</p>
+                  <BsFillBookmarkCheckFill className="text-xl text-white cursor-pointer" />
+                </div>
+              </div>
+            )
+          })}
+        </>
+      )}
     </div>
   )
 }
