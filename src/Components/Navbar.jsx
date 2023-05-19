6c009c016f4a075cd22c13a108330d1902954044
Alfe3n <115562186+Alfe3n@users.noreply.github.com>
2023-05-20 00:21:25 +0530

    small ui changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 function Navbar(props) {
   return (
-    <div className="flex justify-between w-screen pt-12 pb-10 text-xl text-white font md:text-2xl">
+    <div className="flex justify-between w-screen pt-8 pb-10 text-xl text-white font md:text-2xl">
       <Link to="/">
         <p className={`ml-10 md:ml-20 cursor-pointer ${props.home == true ? 'font-extrabold' : 'font-thin'}`}>Home</p>
       </Link>
