157b3417bb7732894097769504346479e52b25ea
Alfe3n <115562186+Alfe3n@users.noreply.github.com>
2023-05-20 00:26:36 +0530

    Added PageNotFound

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import './App.css'
 import Home from './Pages/Home'
 import { Routes, Route } from 'react-router-dom'
 import Bookmark from './Pages/Bookmark'
+import Notfound from './Pages/Notfound'
+
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/bookmarks" element={<Bookmark />} />
+        <Route path="*" element={<Notfound />}></Route>
       </Routes>
     </div>
   )
---
+++ src/Pages/Notfound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+function Notfound() {
+  return (
+    <div className=" items-center h-screen bg-violet flex flex-col">
+      <Navbar />
+      <h1 className="mt-20 text-2xl font-extrabold cursor-pointer text-red md:text-6xl">Page Not Found!</h1>
+    </div>
+  )
+}
+
+export default Notfound
