b61ecee547152ea25d2529d87301767fef35242c
Alfe3n <115562186+Alfe3n@users.noreply.github.com>
2023-05-20 00:29:26 +0530

    final

--- src/Pages/Notfound.jsx
+++ src/Pages/Notfound.jsx
@@ -4,7 +4,7 @@ function Notfound() {
   return (
     <div className=" items-center h-screen bg-violet flex flex-col">
       <Navbar />
-      <h1 className="mt-20 text-2xl font-extrabold cursor-pointer text-red md:text-6xl">Page Not Found!</h1>
+      <h1 className="mt-20 text-2xl font-extrabold  text-red md:text-6xl">Page Not Found!</h1>
     </div>
   )
 }
